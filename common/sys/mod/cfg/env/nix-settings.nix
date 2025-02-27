d3a41f219822c0d08d15b5598a3f5d525c6e2ca0
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-02-27 12:34:43 -0600

    ci: add garnix cache substituter

--- common/sys/mod/cfg/env/nix-settings.nix
+++ common/sys/mod/cfg/env/nix-settings.nix
@@ -8,10 +8,15 @@
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
 
   # Enable flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    substituters = [ "https://cache.garnix.io" ];
+    trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;

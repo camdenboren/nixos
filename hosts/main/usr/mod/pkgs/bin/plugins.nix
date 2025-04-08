e87c973b7b0a2e462db34f439499578ed9fb8b72
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-04-08 08:07:27 -0500

    update: hotfix - mv wine and yabridge to pkgs-stable

--- hosts/main/usr/mod/pkgs/bin/plugins.nix
+++ hosts/main/usr/mod/pkgs/bin/plugins.nix
@@ -1,55 +1,64 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-stable, ... }:
 
 {
-  home.packages = with pkgs; [
-    # Plugins - deps for winvsts
-    wineWowPackages.staging
-    winetricks
-    yabridge
-    yabridgectl
-    # Plugins - custom derivations
-    audiogridder
-    sitala
-    neural-amp-modeler-lv2
-    loudmax
-    # Plugins - windows via yabridge, put in lib/winvst3
-    # try DPI 120 in winecfg if gui is buggy
-    ssq
-    britpressor
-    fetish
-    rare
-    kotelnikov
-    nova
-    # Plugins - external
-    freq-echo
-    span
-    ssd # needs dxvk via winetricks
-    maudioplugins # needs music/Plugin\ Data/MeldaProduction put in ~/.wine/drive_c/ProgramData
-    abstract-vox
-    # Plugins - packs
-    distrho-ports
-    gxplugins-lv2
-    lsp-plugins
-    x42-plugins
-    # Plugins - guitar
-    proteus
-    # Plugins - synth
-    helm
-    odin2
-    cardinal
-    surge-XT
-    zynaddsubfx
-    bespokesynth
-    x42-gmsynth
-    # Plugins - drum
-    x42-avldrums
-    drumgizmo
-    hydrogen
-    # Plugins - other
-    carla
-    rnnoise-plugin
-    noise-repellent
-    sfizz
-    fluidsynth
-  ];
+  home.packages =
+    with pkgs;
+    [
+      # Plugins - deps for winvsts
+      #wineWowPackages.staging
+      #winetricks
+      #yabridge
+      #yabridgectl
+      # Plugins - custom derivations
+      audiogridder
+      sitala
+      neural-amp-modeler-lv2
+      loudmax
+      # Plugins - windows via yabridge, put in lib/winvst3
+      # try DPI 120 in winecfg if gui is buggy
+      ssq
+      britpressor
+      fetish
+      rare
+      kotelnikov
+      nova
+      # Plugins - external
+      freq-echo
+      span
+      ssd # needs dxvk via winetricks
+      maudioplugins # needs music/Plugin\ Data/MeldaProduction put in ~/.wine/drive_c/ProgramData
+      abstract-vox
+      # Plugins - packs
+      distrho-ports
+      gxplugins-lv2
+      lsp-plugins
+      x42-plugins
+      # Plugins - guitar
+      proteus
+      # Plugins - synth
+      helm
+      odin2
+      cardinal
+      surge-XT
+      zynaddsubfx
+      bespokesynth
+      x42-gmsynth
+      # Plugins - drum
+      x42-avldrums
+      drumgizmo
+      hydrogen
+      # Plugins - other
+      carla
+      rnnoise-plugin
+      noise-repellent
+      sfizz
+      fluidsynth
+    ]
+    ++ (with pkgs-stable; [
+      # yabridge is broken on wine versions > 9.21 or so
+      wineWowPackages.staging
+      winetricks
+      yabridge
+      yabridgectl
+    ]);
 }

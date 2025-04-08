80c097d83bfbbbd0c62f6269993a8f96e6daa7df
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-04-08 10:44:30 -0500

    update: bump loudmax plugin from 1.45 -> 1.46

--- hosts/main/usr/drv/loudmax/default.nix
+++ hosts/main/usr/drv/loudmax/default.nix
@@ -2,14 +2,13 @@
 
 stdenv.mkDerivation {
   pname = "loudmax";
-  version = "v1.45";
+  version = "v1.46";
+
   src = fetchzip {
-    url = "https://www.dropbox.com/scl/fi/2s22hrnpdd6w48197gqch/LoudMax_v1_45_Linux_x86_LADSPA.tar.gz?rlkey=h5fvfmx4bdjhiheohx4ogfv2q&dl=1";
-    hash = "sha256-B/vji0NN96hq5COSgjHdLiFzaDwOWv9veZ7TeEvIigs=";
+    url = "https://www.dropbox.com/scl/fi/f4oyldvfs5bc5yzd4dddu/LoudMax_v1_46_Linux_x86_LADSPA.tar.gz?rlkey=ugykl90lzbrq9yl42oryz0b21&st=e5h5aqk6&dl=1";
+    hash = "sha256-jwox6f7FBSnjcugrf+gFijsk0N41dJkyKZgVo+WNQWk=";
     stripRoot = false;
   };
-
-  nativeBuildInputs = [ ];
 
   installPhase = ''
     mkdir -p $out/lib/ladspa

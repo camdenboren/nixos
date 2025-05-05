e1a2c20ae1910da850b9414dfc34ac38596a5244
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-05-05 10:58:11 -0500

    update: nh overridden v4.0.0-beta.9 -> v4.0.3

--- common/sys/mod/pkgs/bin/utils/default.nix
+++ common/sys/mod/pkgs/bin/utils/default.nix
@@ -4,24 +4,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    (nh.overrideAttrs (
-      final: prev: rec {
-        version = "4.0.0-beta.9";
-        src = fetchFromGitHub {
-          owner = "viperML";
-          repo = "nh";
-          tag = "v${version}";
-          hash = "sha256-HcNEFumdZ4Y59CBB1h1IzOyPxwn0u/Wson6hhzItXCA=";
-        };
-
-        preFixup = '''';
-
-        cargoDeps = prev.cargoDeps.overrideAttrs {
-          name = "nh-vendor.tar.gz";
-          inherit (final) src;
-          outputHash = "sha256-tcT/miROl+unG8V9RklXvmOAQ9pe9QeLinicnJjcxwg=";
-        };
-      }
-    ))
+    nh
   ];
 }

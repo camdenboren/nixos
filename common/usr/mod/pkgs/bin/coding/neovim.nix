1243763eff3e11bd7f86251005a2cc530a0a0b2b
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-06-20 21:43:44 -0500

    neovim: add tree + icons, gitgutter, lualine, telescope + map <C-s>; rm ~

--- common/usr/mod/pkgs/bin/coding/neovim.nix
+++ common/usr/mod/pkgs/bin/coding/neovim.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+# don't format this file w/in zed-use nixfmt or fmt directly
+# zed removes the space at the end of eob:\, breaking the removal of ~
+
 {
   programs.neovim = {
     enable = true;
@@ -10,10 +13,22 @@
 
     plugins = with pkgs.vimPlugins; [
       nvim-lspconfig
+      nvim-tree-lua
+      nvim-web-devicons
+      vim-gitgutter
+      lualine-nvim
+      telescope-nvim
     ];
 
     extraLuaConfig = ''
-        require("lspconfig").nixd.setup({
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      require("nvim-tree").setup()
+      require('telescope').setup()
+      require'nvim-web-devicons'.setup()
+      require('lualine').setup()
+      require("lspconfig").nixd.setup({
         cmd = { "nixd" },
         settings = {
           nixd = {
@@ -29,6 +44,9 @@
 
       vim.cmd([[
         set notermguicolors
+        set relativenumber
+        set fillchars=eob:\ 
+        map <C-s> :Telescope find_files<CR>
       ]])
     '';
   };

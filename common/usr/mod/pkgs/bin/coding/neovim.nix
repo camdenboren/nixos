8623fb710225abc129aca01d73aaca1efb0a8391
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-07-03 21:31:36 -0500

    neovim: enable blinking cursor

--- common/usr/mod/pkgs/bin/coding/neovim.nix
+++ common/usr/mod/pkgs/bin/coding/neovim.nix
@@ -57,6 +57,12 @@
         map <C-g> :LazyGit<CR>
         map <C-x> :lua vim.diagnostic.open_float()<CR>
       ]])
+
+      vim.opt.guicursor = {
+        'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+        'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+        'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+      }
     '';
   };
 }

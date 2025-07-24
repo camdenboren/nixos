7280b39959c783a252859ae763213590fe15df14
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-07-24 10:35:49 -0500

    freetube: rm `value` from `bounds`, rp `mkIf` -> `optionalAttrs`
    Should fix
    - FreeTube not launching
    - Unwanted settings being embedded into json via mkIf

--- common/usr/mod/pkgs/bin/basic/freetube.nix
+++ common/usr/mod/pkgs/bin/basic/freetube.nix
@@ -14,9 +14,9 @@ in
     package = lib.mkIf isDarwin null;
 
     # doesn't put settings.db in correct location on mac, so it's copied in replaceConfigs.nix
-    settings = {
-      bounds = {
-        value = {
+    settings =
+      {
+        bounds = {
           x = if isDarwin then 136 else 925;
           y = if isDarwin then 254 else 346;
           width = if isDarwin then 2288 else 723;
@@ -24,38 +24,41 @@ in
           maximized = true;
           fullScreen = false;
         };
+        checkForUpdates = false;
+        checkForBlogPosts = false;
+        hideHeaderLogo = true;
+        mainColor = if (hostname == "media") then "Teal" else "Red";
+        secColor = if (hostname == "media") then "CatppuccinFrappeTeal" else "Red";
+        autoplayVideos = false;
+        defaultQuality = 1080;
+        fetchSubscriptionsAutomatically = false;
+        saveWatchedProgress = false;
+        rememberHistory = false;
+        useSponsorBlock = true;
+        sponsorBlockShowSkippedToast = false;
+        sponsorBlockSelfPromo = {
+          color = "Yellow";
+          skip = "autoSkip";
+        };
+        sponsorBlockInteraction = {
+          color = "Green";
+          skip = "doNothing";
+        };
+        uiScale = if isDarwin then 110 else 125;
+        defaultViewingMode = "theatre";
+        displayVideoPlayButton = false;
+        externalPlayer = "mpv";
+        rememberSearchHistory = false;
+        baseTheme = "black";
+        hideLabelsSideBar = true;
+        hideActiveSubscriptions = true;
+        enableSubtitlesByDefault = true;
+      }
+      // lib.optionalAttrs (hostname == "media") {
+        defaultProfile = "C8kpmytTeH7aRdKy";
+      }
+      // lib.optionalAttrs isDarwin {
+        externalPlayerExecutable = "/Users/camdenboren/.nix-profile/bin/mpv";
       };
-      checkForUpdates = false;
-      checkForBlogPosts = false;
-      hideHeaderLogo = true;
-      mainColor = if (hostname == "medial") then "Teal" else "Red";
-      secColor = if (hostname == "media") then "CatppuccinFrappeTeal" else "Red";
-      autoplayVideos = false;
-      defaultQuality = 1080;
-      fetchSubscriptionsAutomatically = false;
-      saveWatchedProgress = false;
-      rememberHistory = false;
-      useSponsorBlock = true;
-      sponsorBlockShowSkippedToast = false;
-      sponsorBlockSelfPromo = {
-        color = "Yellow";
-        skip = "autoSkip";
-      };
-      sponsorBlockInteraction = {
-        color = "Green";
-        skip = "doNothing";
-      };
-      uiScale = if isDarwin then 110 else 125;
-      defaultProfile = lib.mkIf (hostname == "media") "C8kpmytTeH7aRdKy";
-      defaultTheatreMode = true;
-      displayVideoPlayButton = false;
-      externalPlayer = "mpv";
-      externalPlayerExecutable = lib.mkIf isDarwin "/Users/camdenboren/.nix-profile/bin/mpv";
-      rememberSearchHistory = false;
-      baseTheme = "black";
-      hideLabelsSideBar = true;
-      hideActiveSubscriptions = true;
-      enableSubtitlesByDefault = true;
-    };
   };
 }

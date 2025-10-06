473107b95421e51a5195f3bee71b9d1e6b1aecac
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-10-06 14:23:15 -0500

    update: mv carla -> unstable

--- hosts/main/usr/mod/pkgs/bin/plugins.nix
+++ hosts/main/usr/mod/pkgs/bin/plugins.nix
@@ -48,7 +48,7 @@
       drumgizmo
       hydrogen
       # Plugins - other
-      #carla
+      carla
       rnnoise-plugin
       noise-repellent
       sfizz
@@ -60,8 +60,5 @@
       winetricks
       yabridge
       yabridgectl
-
-      # carla is broken, see NixOS/nixpkgs#437077
-      carla
     ]);
 }

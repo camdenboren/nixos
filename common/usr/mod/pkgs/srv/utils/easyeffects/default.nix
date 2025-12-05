b3d604dee654c3a9864bd2a4da5e42160caca282
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-12-05 13:56:13 -0600

    update: correct ezfx preset cfg for main
    This wasn't ever working correctly for main since I was referencing
    `system` rather than `hostname` in the corresponding module. There was
    also a typo in `HF_KEF_AKG.nix`, which has also been corrected. It
    doesn't seem like presets are autoloaded w/o first selecting in the UI,
    however

--- common/usr/mod/pkgs/srv/utils/easyeffects/default.nix
+++ common/usr/mod/pkgs/srv/utils/easyeffects/default.nix
@@ -1,9 +1,9 @@
-{ system, ... }:
+{ hostname, ... }:
 
 let
-  preset = if (system == "main") then "HF_KEG_AKG" else "Pioneer";
+  preset = if (hostname == "main") then "HF_KEF_AKG" else "Pioneer";
   extraPresets =
-    if (system == "main") then
+    if (hostname == "main") then
       {
         hf-kef-akg = (import ./HF_KEF_AKG.nix);
       }

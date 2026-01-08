e278ebbc9dad9c4584c1eca7ba4085164b62e4ce
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-01-08 10:25:21 -0600

    update: rm `--disable-gpu` for tidal-hifi

--- common/usr/mod/pkgs/bin/basic/default.nix
+++ common/usr/mod/pkgs/bin/basic/default.nix
@@ -24,31 +24,7 @@ in
       high-tide
       lollypop
       qbittorrent
-      (tidal-hifi.overrideAttrs {
-        desktopItems = [
-          (makeDesktopItem {
-            exec = "tidal-hifi --disable-gpu";
-            name = "TIDAL Hi-Fi";
-            desktopName = "tidal-hifi";
-            genericName = "TIDAL Hi-Fi";
-            comment = "The web version of listen.tidal.com running in electron with hifi support thanks to widevine.";
-            icon = "tidal-hifi";
-            startupNotify = true;
-            terminal = false;
-            type = "Application";
-            categories = [
-              "Network"
-              "Application"
-              "AudioVideo"
-              "Audio"
-              "Video"
-            ];
-            startupWMClass = "tidal-hifi";
-            mimeTypes = [ "x-scheme-handler/tidal" ];
-            extraConfig.X-PulseAudio-Properties = "media.role=music";
-          })
-        ];
-      })
+      tidal-hifi
       vlc
     ]
     ++ lib.optionals (!isLinux) [

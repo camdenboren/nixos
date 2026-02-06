3e42172e631597af137028e93a81fbdcd04b02a3
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-02-06 11:22:19 -0600

    update: set `inputs.nixpkgs.follows = "nixpkgs"` for chat-script

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
     };
     chat-script = {
       url = "github:camdenboren/chat-script";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     chatbot-util = {
       url = "github:collegiate-edu-nation/chatbot-util";

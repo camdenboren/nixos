ce371e5129b573ff0ecd3cbefc5e9bc01328d2b0
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-02-06 21:34:20 -0600

    update: add penpot update cmds

--- hosts/media/sys/mod/pkgs/srv/default.nix
+++ hosts/media/sys/mod/pkgs/srv/default.nix
@@ -2,6 +2,24 @@
 
 # generate penpot drv after cd'ing here via
 # $(curl -o docker-compose.yml https://raw.githubusercontent.com/penpot/penpot/refs/heads/develop/docker/images/docker-compose.yaml && nix run github:aksiksi/compose2nix -- -project=penpot && mv docker-compose.nix penpot.nix && tr docker-compose.yml && fmt)
+#
+# update via
+#
+# shell jq
+#
+# sudo podman pull $(sudo podman inspect penpot-penpot-postgres | jq -r .[0].ImageName)
+# sudo podman pull $(sudo podman inspect penpot-penpot-mailcatch | jq -r .[0].ImageName)
+# sudo podman pull $(sudo podman inspect penpot-penpot-valkey | jq -r .[0].ImageName)
+# sudo podman pull $(sudo podman inspect penpot-penpot-exporter | jq -r .[0].ImageName)
+# sudo podman pull $(sudo podman inspect penpot-penpot-backend | jq -r .[0].ImageName)
+# sudo podman pull $(sudo podman inspect penpot-penpot-frontend | jq -r .[0].ImageName)
+#
+# sudo systemctl restart podman-penpot-penpot-postgres.service
+# sudo systemctl restart podman-penpot-penpot-mailcatch.service
+# sudo systemctl restart podman-penpot-penpot-valkey.service
+# sudo systemctl restart podman-penpot-penpot-exporter.service
+# sudo systemctl restart podman-penpot-penpot-backend.service
+# sudo systemctl restart podman-penpot-penpot-frontend.service
 
 {
   imports = [

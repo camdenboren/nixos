afe4e8c8bedff312dc8b308202a79c858818b0fa
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-03-07 23:01:01 -0600

    travel: set shutdown script via `mkForce`

--- common/sys/mod/cfg/env/travel.nix
+++ common/sys/mod/cfg/env/travel.nix
@@ -1,11 +1,11 @@
-{ ... }:
+{ lib, ... }:
 
 {
   specialisation = {
     travel.configuration = {
-      systemd.services."shutdown".script = ''
+      systemd.services."shutdown".script = lib.mkForce ''
         set -eu
-        echo "shutdown disabled by `travel` specialisation, doing nothing..."
+        echo "shutdown disabled by 'travel' specialisation, doing nothing..."
       '';
     };
   };

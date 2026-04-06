372ac7b54744fe644c8ba944b6780a949bfe53ed
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-04-06 15:45:12 -0500

    update: bump bentopdf from 2.4.0 -> 2.8.2

--- hosts/media/sys/drv/bentopdf/default.nix
+++ hosts/media/sys/drv/bentopdf/default.nix
@@ -2,24 +2,21 @@
 
 buildNpmPackage rec {
   pname = "bentopdf";
-  version = "2.4.0";
+  version = "2.8.2";
   src = fetchFromGitHub {
     owner = "alam00000";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-nzrVLs0x/mRCEtTOXXuhbWd2BJAOI262BcZ8r4eJdnw=";
+    hash = "sha256-ZSBO+EO+ZKLqWjTHhKvfqJnbDO1S8UY2bZLaKoQKyqo=";
   };
 
-  # regenerate via
-  # 1. Running: $(npm rm embedpdf-snippet && npm install embedpdf-snippet-i18n@2.3.0-1)
-  # 2. Replacing all instances of `embedpdf-snippet` -> `embedpdf-snippet-i18n`
-  #   a. `src/js/logic/edit-pdf-page.ts` has two offenders
-  #   b. `vite.config.ts` has one
-  # 3. Regenerating patch (don't add changes) $(git diff > unvendored-deps.patch)
+  # regenerate in most recent repo tag via
+  # 1. $(shell nodejs gnused)
+  # 2. $(npm rm embedpdf-snippet && npm i @embedpdf/snippet@2.9.1 && sed -i 's|embedpdf-snippet|@embedpdf/snippet|g' src/js/logic/edit-pdf-page.ts && sed -i 's|embedpdf-snippet|@embedpdf/snippet|g' vite.config.ts && git diff > unvendored-deps.patch && cp -f unvendored-deps.patch ~/etc/nixos/hosts/media/sys/drv/bentopdf/)
   patches = [ ./unvendored-deps.patch ];
 
   npmFlags = [ "--legacy-peer-deps" ];
-  npmDepsHash = "sha256-GTooExkMYt2wlBJA0qHt2ZrqmHQnwYmW3w/l9U8FNyQ=";
+  npmDepsHash = "sha256-+RwEq2nnECD0R9uuyDUgYs0BwPvvYTt17g2RCRUSqJ8=";
 
   # setting `env.SIMPLE_MODE = true;` didn't work
   buildPhase = ''

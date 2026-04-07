8f312b5bffa5c67455c25720715bdb7a8678c1e4
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-04-07 12:16:07 -0500

    update: bump maudioplugins from 17.07.04 -> 17.08.01

--- hosts/main/usr/drv/maudioplugins/default.nix
+++ hosts/main/usr/drv/maudioplugins/default.nix
@@ -2,13 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "maudioplugins";
-  version = "17.07.04";
+  version = "17.08.01";
   src = fetchzip {
-    url = "https://www.dropbox.com/scl/fi/l0qgcbz59cizs8wqu349v/maudioplugins-${version}.tar.gz?rlkey=72odhz9hp45t1ptimjlf82imp&st=h4ag092x&dl=1";
-    hash = "sha256-Tq2yUV/cPsZAF+iZCNhHys08X5R8bFUGEErkglcB4UY=";
+    url = "www.dropbox.com/scl/fi/xbiw2jd86gcc4q3sv1z4x/maudioplugins-${version}.tar.gz?rlkey=6fbfbnh9tn1jexni29qd3u3i5&st=ukcrz1e9&dl=1";
+    hash = "sha256-/N0HhUgCfNEIDf7jJNXQT0EexB/uuAZdAhyaN/ND0+w=";
     stripRoot = false;
   };
-  # installer url = "https://www.meldaproduction.com/downloads/down?name=offline%2Fmaudioplugins_17_07_04.zip&platform=offline&version=17_07_04&mirror=bunnycdn&url=https%3A%2F%2Fmeldaproduction.b-cdn.net%2Fdownload%2Foffline%2Fmaudioplugins_17_07_04.zip&checksum=1a711ccaf476af6ef120148ceafd48cf571baf36";
 
   installPhase = ''
     mkdir -p $out/lib/winvst3

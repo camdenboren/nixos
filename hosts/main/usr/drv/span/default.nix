cbb4db4a8038890e4b770b57457391053d0b5d0e
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-04-07 11:46:56 -0500

    update: bump span from 3.23 -> 3.24

--- hosts/main/usr/drv/span/default.nix
+++ hosts/main/usr/drv/span/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "span";
-  version = "3.23";
+  version = "3.24";
   src = fetchzip {
-    url = "https://www.dropbox.com/scl/fi/hesfgdzzccsabvyhuvtc5/span-${version}.tar.gz?rlkey=fbpp9ajg2vd82dmziek5b9rd3&st=r7zjgxo4&dl=1";
-    hash = "sha256-91hUz0WDbz7kzT2BbEhfRgxmunNhnzIkWiBsHyVzTwY=";
+    url = "https://www.dropbox.com/scl/fi/ssytkknumojc0p87zuyce/span-${version}.tar.gz?rlkey=lboa5ig3ubsvlyv18stlnzhby&st=m5i01973&dl=1";
+    hash = "sha256-6/LKon/WrgvSOYXFpYE14rBgnP8GMkBLEFwh3jh0XnA=";
     stripRoot = false;
   };
-  # installer url = "https://www.voxengo.com/files/VoxengoSPAN_323_Win32_64_VST_VST3_AAX_setup.exe";
+  # installer url = "https://www.voxengo.com/files/VoxengoSPAN_324_Win32_64_VST_VST3_AAX_setup.exe";
 
   installPhase = ''
     mkdir -p $out/lib/winvst3

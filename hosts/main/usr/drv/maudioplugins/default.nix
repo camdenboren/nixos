a7902febe1aaec6348fc90a7ab8b5bbabdb2239c
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-05-05 12:44:56 -0500

    update: bump maudioplugins from 17.08.01 -> 17.08.02

--- hosts/main/usr/drv/maudioplugins/default.nix
+++ hosts/main/usr/drv/maudioplugins/default.nix
@@ -2,9 +2,9 @@
 
 stdenv.mkDerivation rec {
   pname = "maudioplugins";
-  version = "17.08.01";
+  version = "17.08.02";
   src = fetchzip {
-    url = "www.dropbox.com/scl/fi/xbiw2jd86gcc4q3sv1z4x/maudioplugins-${version}.tar.gz?rlkey=6fbfbnh9tn1jexni29qd3u3i5&st=ukcrz1e9&dl=1";
+    url = "www.dropbox.com/scl/fi/gca9nr38rwg7rokydh3xm/maudioplugins-${version}.tar.gz?rlkey=qozv5cczgn6xbxdhbzdrsd0v1&st=rmkv2zl8&dl=1";
     hash = "sha256-/N0HhUgCfNEIDf7jJNXQT0EexB/uuAZdAhyaN/ND0+w=";
     stripRoot = false;
   };

fdf1b37a791b595e1b7f39c78a627b8e50e80388
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-06-10 09:17:23 -0500

    ci: various caching improvements
    - Only hash `flake.lock` for the primary key to increase the GHA cache
    utilization between runs.
    - Added `cache.nixos-cuda.org` binary cache to reduce build times on
    main.
    - Added all binary caches to runners to reduce CI build times

--- common/sys/mod/cfg/env/nix-settings.nix
+++ common/sys/mod/cfg/env/nix-settings.nix
@@ -16,10 +16,12 @@
       substituters = [
         "https://camdenboren.cachix.org"
         "https://edu-nation.cachix.org"
+        "https://cache.nixos-cuda.org"
       ];
       trusted-public-keys = [
         "camdenboren.cachix.org-1:gjOBUYw06+i6CJIjfmVJ+ASrVLoEoOOn/2d6XcQkiFA="
         "edu-nation.cachix.org-1:S2s7ZDuLeFrV2qhfzXWNt+/XlnGxUjvUHv0WI+BvM+0="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
     };
   };

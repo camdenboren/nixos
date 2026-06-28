e51982bed507d652e3c84d824f9dbd8cb16bc523
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-06-28 10:09:41 -0500

    update: bump bentopdf from 2.8.5 -> 2.8.6

--- hosts/media/sys/drv/bentopdf/default.nix
+++ hosts/media/sys/drv/bentopdf/default.nix
@@ -2,12 +2,12 @@
 
 buildNpmPackage rec {
   pname = "bentopdf";
-  version = "2.8.5";
+  version = "2.8.6";
   src = fetchFromGitHub {
     owner = "alam00000";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-/Jzhi291hxsyBGde72wKXkk6TSBMKUSmCdVtLWrvBzQ=";
+    hash = "sha256-rbThEonDXFGcudgdMtDrQHq84Wh4IvOZZBn4kXvrhoI=";
   };
 
   # regenerate in most recent repo tag via
@@ -16,7 +16,7 @@ buildNpmPackage rec {
   patches = [ ./unvendored-deps.patch ];
 
   npmFlags = [ "--legacy-peer-deps" ];
-  npmDepsHash = "sha256-ImWFzz99oE8dQtHK7MVBJZ4pXO1QDAjqkINzcVNsGmM=";
+  npmDepsHash = "sha256-KVaWR59xdG20qMo+tqVsKfMzDxXWWkxRmi8yaSjT82o=";
 
   # setting `env.SIMPLE_MODE = true;` didn't work
   buildPhase = ''

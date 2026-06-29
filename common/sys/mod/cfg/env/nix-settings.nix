01ca65e628142c3866482751ad71359c4e74c0bf
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-06-29 12:23:41 -0500

    ssh: add `camdenboren` to `trusted-users` to enable remote builds
    Missed this in 2186ace0
    
    Room for improvement here on using a dedicated user for this w/ more
    granular privileges (i.e., `remotebuild`)

--- common/sys/mod/cfg/env/nix-settings.nix
+++ common/sys/mod/cfg/env/nix-settings.nix
@@ -1,8 +1,14 @@
 {
   inputs,
+  lib,
+  system,
+  hostname,
   ...
 }:
 
+let
+  needsBuilder = system == "x86_64-linux" && hostname != "main";
+in
 {
   nix = {
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
@@ -13,11 +19,24 @@
         "flakes"
       ];
 
+      # prevents `error: cannot add path... because it lacks a signature by a trusted key`
+      # when copying paths from main
+      #
+      # i don't set this for all hosts since there's security implications here re.
+      # root access https://wiki.nixos.org/wiki/Distributed_build
+      #
+      # TODO: a more fine-grained approach will likely be a good idea in the future (i.e.,
+      # remotebuild user per https://nix.dev/tutorials/nixos/distributed-builds-setup.html)
+      trusted-users = lib.optionals needsBuilder [
+        "camdenboren"
+      ];
+
       substituters = [
         "https://camdenboren.cachix.org"
         "https://edu-nation.cachix.org"
         "https://cache.nixos-cuda.org"
       ];
+
       trusted-public-keys = [
         "camdenboren.cachix.org-1:gjOBUYw06+i6CJIjfmVJ+ASrVLoEoOOn/2d6XcQkiFA="
         "edu-nation.cachix.org-1:S2s7ZDuLeFrV2qhfzXWNt+/XlnGxUjvUHv0WI+BvM+0="

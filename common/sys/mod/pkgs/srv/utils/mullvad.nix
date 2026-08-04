80389b746c6a75b8996554d4b041bd6e955c0601
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-08-04 10:35:35 -0500

    update: enable mullvad gui
    The gui is now separated from the daemon as nixpkgs is finally building
    it from source NixOS/nixpkgs#414831 - this might also enable patching
    the app to allow simultaneous usage of tailscale + mullvad, which
    previously would've required firewall configurations that eluded me :)

--- common/sys/mod/pkgs/srv/utils/mullvad.nix
+++ common/sys/mod/pkgs/srv/utils/mullvad.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+_:
 
 {
   # Re-add to autostart application in Gnome tweaks when updated to fix out-of-sync error
   services.mullvad-vpn = {
     enable = true;
-    package = pkgs.mullvad-vpn;
+    gui.enable = true;
   };
 }

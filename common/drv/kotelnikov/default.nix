4e9346bd455a5b880053cb7dfeef486f14341e27
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-08-25 10:00:00 -0500

    updatePkgs: match kotelnikov's zip handling to nova's
    Was causing a hash mismatch

--- common/drv/kotelnikov/default.nix
+++ common/drv/kotelnikov/default.nix
@@ -5,13 +5,12 @@ stdenv.mkDerivation rec {
   version = "1.6.7";
   src = fetchzip {
     url = "https://www.tokyodawn.net/labs/Kotelnikov/${version}/TDR%20Kotelnikov%20(no%20installer).zip";
-    hash = "sha256-sKjy4AZaEDpthKn8ndqK8TjxIuUIpoCYGk+JCZRdYPU=";
-    stripRoot = false;
+    hash = "sha256-CxRYeBe+t5jN1pNLXHy7KUyzdTZ0oy7E+lK02sZyN7s=";
   };
 
   installPhase = ''
     mkdir -p $out/lib/winvst3
-    cp -r TDR\ Kotelnikov\ \(no\ installer\)/VST3/x64/TDR\ Kotelnikov.vst3 $out/lib/winvst3
+    cp -r VST3/x64/TDR\ Kotelnikov.vst3 $out/lib/winvst3
   '';
 
   passthru.updateScript = [

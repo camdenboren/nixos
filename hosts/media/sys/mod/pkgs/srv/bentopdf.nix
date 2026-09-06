86622893a663e654b47579c573686932b2ac8c86
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2026-09-06 12:13:51 -0500

    bentopdf: rm pkg override

--- hosts/media/sys/mod/pkgs/srv/bentopdf.nix
+++ hosts/media/sys/mod/pkgs/srv/bentopdf.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+_:
 
 let
   baseDomain = "home.local";
@@ -6,21 +6,6 @@ in
 {
   services.bentopdf = {
     enable = true;
-    package = pkgs.bentopdf.overrideAttrs (
-      finalAttrs: prevAttrs: {
-        version = "2.8.7";
-        src = prevAttrs.src.override {
-          tag = "v${finalAttrs.version}";
-          hash = "sha256-SduYMgRs5IOLNJx1tHCp/UpUhB8vDzRfLY0ZzTUQrQI=";
-        };
-        npmDepsHash = "sha256-nKAny59NUuT45GZtXyUVihxbbFYvu1t3PVDBX8gPPws=";
-        npmDeps = pkgs.fetchNpmDeps {
-          inherit (finalAttrs) src;
-          name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
-          hash = finalAttrs.npmDepsHash;
-        };
-      }
-    );
     domain = "pdf.${baseDomain}";
     nginx = {
       enable = true;

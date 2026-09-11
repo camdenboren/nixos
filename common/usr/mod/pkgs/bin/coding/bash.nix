camdenboren/nixos

commit d52ca9f994a762771123f8ee266bd942508462b0

ollama: correct env var for preventing history storage
The incorrect variable was being used AND it only applies to client
invocations like `ollama run ...`, so it needed to be set in `bash.nix`
instead

common/usr/mod/pkgs/bin/coding/bash.nix
@@ -10,6 +10,7 @@ let
   isDarwin = lib.hasSuffix "-darwin" system;
   cfgKey = if isDarwin then "darwin" else "nixos";
   isVM = lib.hasSuffix "vm" hostname;
+  isOllamaServer = hostname == "main" || hostname == "media";
   needsBuilder = system == "x86_64-linux" && hostname != "main";
 in
 {
@@ -101,45 +102,49 @@ in
         { }
     );
 
-    initExtra = ''
-      if (( SHLVL > 1 )); then
-        export PS1="\n\[\033[1;31m\][shell:\w]\$\[\033[0m\] "
-      fi
+    initExtra =
+      lib.optionals isOllamaServer ''
+        export OLLAMA_NOHISTORY=1
+      ''
+      + ''
+        if (( SHLVL > 1 )); then
+          export PS1="\n\[\033[1;31m\][shell:\w]\$\[\033[0m\] "
+        fi
 
-      mkcd () {
-        mkdir $1
-        cd $1
-      }
+        mkcd () {
+          mkdir $1
+          cd $1
+        }
 
-      nix () {
-        case "$1" in
-          build|shell|develop|copy|flake)
-            ${pkgs.nix-output-monitor}/bin/nom "$@"
-          ;;
-          *)
-            command nix "$@"
-          ;;
-        esac
-      }
+        nix () {
+          case "$1" in
+            build|shell|develop|copy|flake)
+              ${pkgs.nix-output-monitor}/bin/nom "$@"
+            ;;
+            *)
+              command nix "$@"
+            ;;
+          esac
+        }
 
-      run () {
-        nix run nixpkgs#$1
-      }
+        run () {
+          nix run nixpkgs#$1
+        }
 
-      shell () {
-        NIXPKGS_STRING=""
-        for var in "$@"
-        do
-          VAR_STRING="nixpkgs#''${var} "
-          NIXPKGS_STRING+=$VAR_STRING
-        done
-        nix shell $NIXPKGS_STRING
-      }
+        shell () {
+          NIXPKGS_STRING=""
+          for var in "$@"
+          do
+            VAR_STRING="nixpkgs#''${var} "
+            NIXPKGS_STRING+=$VAR_STRING
+          done
+          nix shell $NIXPKGS_STRING
+        }
 
-      updatePkg () {
-        ${pkgs.nix-update}/bin/nix-update \
-          nixosConfigurations.main.pkgs."$@" --flake
-      }
-    '';
+        updatePkg () {
+          ${pkgs.nix-update}/bin/nix-update \
+            nixosConfigurations.main.pkgs."$@" --flake
+        }
+      '';
   };
 }

common/usr/mod/pkgs/srv/utils/ollama.nix
@@ -16,7 +16,8 @@
       OLLAMA_NUM_PARALLEL = "4";
       OLLAMA_MAX_QUEUE = "512";
       OLLAMA_CONTEXT_LENGTH = "65536";
-      OLLAMA_KEEP_HISTORY = "0";
+      # `OLLAMA_NOHISTORY=1` is also set in `bash.nix` since it only applies
+      # to CLI invocations of `ollama run`
     };
   };
 }

commit b7ed012c83553fd085b4ebf66c4abbf86e559ef2

bash: rp `lib.optionals` -> `lib.optionalString`
Should fix the build failures introduced in d52ca9f

common/usr/mod/pkgs/bin/coding/bash.nix
@@ -108,7 +108,7 @@ in
     );
 
     initExtra =
-      lib.optionals isOllamaServer ''
+      lib.optionalString isOllamaServer ''
         export OLLAMA_NOHISTORY=1
       ''
       + ''